9ac373414d3f5f27d25ccb290c16ac44d167e9ba
Depozit <maxtop1924@yandex.ru>
2024-06-10 23:19:27 +0300

    -

--- QT/PolyFlow/PolyFlow/main.cpp
+++ QT/PolyFlow/PolyFlow/main.cpp
@@ -1,18 +1,37 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include <qapplication.h>
+#include <qwidget.h>
+#include <qlabel.h>
+#include <qstring.h>
+#include <qpushbutton.h>
+#include <qtranslator.h>
+#include <qgridlayout.h>
 
-int main(int argc, char *argv[])
-{
-#if defined(Q_OS_WIN) && QT_VERSION_CHECK(5, 6, 0) <= QT_VERSION && QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
+int main(int argc, char* argv[]) {
 
-    QGuiApplication app(argc, argv);
+	QApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/polyflow/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
+	QWidget mainFrame;
 
-    return app.exec();
+	mainFrame.setWindowTitle("PolyFlow");
+	mainFrame.setMinimumWidth(1280);
+	mainFrame.setMinimumHeight(720);
+	
+	QPalette testColor;
+	testColor.setColor(QPalette::Active,QPalette::ColorRole::ButtonText, QColor(150,0,250));
+
+	QPushButton testButton{ "test" };
+	testButton.setPalette(testColor);
+	testButton.setAutoFillBackground(true);
+
+	QPushButton testButton2("sheet", &mainFrame);
+
+	QGridLayout g(&mainFrame);
+	g.addWidget(&testButton,0,0);
+	g.addWidget(&testButton2,1,1);
+
+
+
+	mainFrame.show();
+	return app.exec();
+
 }
